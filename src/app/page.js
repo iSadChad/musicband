c780d04448c0086289acfc4c3e75bc5e6386b940
SadTset <tenzin.sharlhey@lernende.bbw.ch>
2025-12-18 16:51:35 +0100

    hmmm

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,31 @@
-"use client";
 
+import "bootstrap/dist/css/bootstrap.css";
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import "./globals.css";
 
-
-function ShapeExample() {
+export default function HomePage() {
   return (
-    <Container>
+    <Container className='mt-5'>
+      <Row className='mt-5'>
+        <Col>
+          <Image src="/pics/newjeans1.jpg" rounded className='w-50 mt-5'/>
+        </Col>
+      </Row>
       <Row>
-        <Col xs={6} md={4}>
-          <Image src="newjeans1.jpg" height={500} rounded />
+        <Col className='mt-5'>
+            <h1 className='mt-5'>NewJeans</h1>
+          <p>NewJeans is a South Korean girl group formed by ADOR, a subsidiary of HYBE Corporation. The group consists of five members: Minji, Hanni, Danielle, Haerin, and Hyein. They officially debuted on August 1, 2022, with the release of their self-titled EP "New Jeans," which includes the hit singles "Attention" and "Hype Boy."</p> 
         </Col>
-        <Col xs={6} md={4}>
-          <Image src="newjeans2.jpg" height={500} rounded />
-        </Col>
-        <Col xs={6} md={4}>
-          <Image src="newjeans3.jpg" height={500} rounded />
+        <Col className='text-center mt-5'>
+        <Image src="/pics/newjeans2.jpg" rounded className='w-100 mt-5'/>
         </Col>
       </Row>
     </Container>
   );
 }
 
-export default ShapeExample;
+
+  
